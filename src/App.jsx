7b9e602289cf0409227623f2ab2317681d922f9e
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-12-29 04:59:31 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,43 @@ import moment from "moment";
 
 const App = () => {
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const [imageKey, setImageKey] = useState(0);
+  const [randomQuote, setRandomQuote] = useState("");
+  const [github, setGithub] = useState({});
 
   useEffect(() => {
+    fetch("https://api.github.com/users/RedowanSajeeb")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setGithub(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching quote:", error);
+      });
+
     const timer = setInterval(() => {
       setTimeLeft(calculateTimeLeft());
     }, 1000);
 
-    return () => clearInterval(timer);
+    // Reload the image every 5 seconds
+    const imageReloadInterval = setInterval(() => {
+      setImageKey((prevKey) => prevKey + 1);
+    }, 5000);
+
+    // Fetch a random quote initially
+    fetchRandomQuote();
+
+    // Fetch a new random quote every 5 seconds
+    const quoteReloadInterval = setInterval(() => {
+      fetchRandomQuote();
+    }, 5000);
+
+    return () => {
+      clearInterval(timer);
+      clearInterval(imageReloadInterval);
+      clearInterval(quoteReloadInterval);
+    };
   }, []);
 
   function calculateTimeLeft() {
@@ -29,6 +59,17 @@ const App = () => {
       seconds: seconds,
       endDate: endDate.format("MMMM D, YYYY HH:mm:ss"),
     };
+  }
+
+  function fetchRandomQuote() {
+    fetch("https://api.quotable.io/quotes/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setRandomQuote(data[0].content);
+      })
+      .catch((error) => {
+        console.error("Error fetching quote:", error);
+      });
   }
 
   return (
@@ -68,7 +109,52 @@ const App = () => {
           </div>
         </div>
       </div>
-      <p className="text-center pb-52 font-bold  font-mono  text-yellow-50">{`End Date: ${timeLeft.endDate}`}</p>
+      <p className="text-center font-bold font-mono text-yellow-50">{`End Date: ${timeLeft.endDate}`}</p>
+
+      {/* Display the random quote */}
+      <h4 className="text-center text-lg text-white mt-5">{randomQuote}</h4>
+      {/* Reload the image every 5 seconds */}
+      <img
+        className="mx-auto mt-5 h-80 md:h-96 rounded-2xl "
+        key={imageKey}
+        src={`https://random.imagecdn.app/500/150?${imageKey}`}
+        alt="Random Image"
+      />
+      <div className="text-yellow-50 space-y-3 pb-10">
+        <div className="flex  items-center justify-center gap-2 mt-10">
+          <img
+            className="h-24 rounded-full"
+            src={github?.avatar_url}
+            alt="Redowan Sajeeb Github avatar"
+          />
+          <div>
+            <h1 className="text-2xl ">{github.name}</h1>
+            <div>
+              <div className="mt-2 mb-2 space-x-3">
+                <a target="blank" className=" text-white" href={github?.blog}>
+                  Blog🌍
+                </a>
+                <a
+                  target="blank"
+                  className=" text-white"
+                  href={github?.html_url}
+                >
+                  gitHub Link👨‍💻
+                </a>
+              </div>
+              <p className="text-sm text-gray-100">
+                Github followers: {github?.followers}
+              </p>
+              <p className="text-sm text-gray-100">
+                public_repos : {github?.public_repos}
+              </p>
+              <p className="text-sm text-gray-100">
+                twitter_username : {github?.twitter_username}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
