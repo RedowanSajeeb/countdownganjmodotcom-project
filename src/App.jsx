834053cc48e478f90ff5ab7b65903267139c4d95
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-12-29 03:29:05 +0600

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import gft from "./assets/ganjhat.gif";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="ms-5 me-5">
+      <img className="mx-auto h-28 rounded-lg pt-2" src={gft} alt="" />
+      <h1 className="text-center  pt-3 text-xl md:text-3xl font-medium text-yellow-50   ">
+        {`Comming Soon Ganjmo`}
+      </h1>
+      <p className="text-center text-sm  mt-2 text-yellow-50 ">
+        {`Welcome to Ganjmo! We're excited to announce that our enhanced e-commerce features are set to launch in just 10-15 days. Get ready for a seamless and powerful online shopping experience.`}
       </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
