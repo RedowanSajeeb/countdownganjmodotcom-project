b146f8e8108d80e3128cf0e37183c591d675b88e
RedowanSajeeb <redowansajeeb72@gmail.com>
2024-04-09 06:16:15 +0600

    ganjmo-logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,184 +1,64 @@
-import React, { useState, useEffect } from "react";
-import gft from "./assets/ganjhat.gif";
-import moment from "moment";
-import { LinkPreview } from "@dhaiwat10/react-link-preview";
-import { SuperSEO } from "react-super-seo";
+import React from "react";
+import posterImg from "./assets/media/ganjmoEidProfile24-01.jpg";
+import logoGanjmo from "./assets/media/ganjmo_web_logo-01.png";
 
 const App = () => {
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-  const [imageKey, setImageKey] = useState(0);
-  const [randomQuote, setRandomQuote] = useState("");
-  const [github, setGithub] = useState({});
-
-  useEffect(() => {
-    fetch("https://api.github.com/users/RedowanSajeeb")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setGithub(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching quote:", error);
-      });
-
-    const timer = setInterval(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
-
-    // Reload the image every 5 seconds
-    const imageReloadInterval = setInterval(() => {
-      setImageKey((prevKey) => prevKey + 1);
-    }, 5000);
-
-    // Fetch a random quote initially
-    fetchRandomQuote();
-
-    // Fetch a new random quote every 5 seconds
-    const quoteReloadInterval = setInterval(() => {
-      fetchRandomQuote();
-    }, 5000);
-
-    return () => {
-      clearInterval(timer);
-      clearInterval(imageReloadInterval);
-      clearInterval(quoteReloadInterval);
-    };
-  }, []);
-
-  function calculateTimeLeft() {
-    const now = moment();
-    const endDate = moment("2024-03-26");
-    const duration = moment.duration(endDate.diff(now));
-    const days = Math.floor(duration.asDays());
-    const hours = duration.hours();
-    const minutes = duration.minutes();
-    const seconds = duration.seconds();
-
-    return {
-      days: days,
-      hours: hours,
-      minutes: minutes,
-      seconds: seconds,
-      endDate: endDate.format("MMMM D, YYYY HH:mm:ss"),
-    };
-  }
-
-  function fetchRandomQuote() {
-    fetch("https://api.quotable.io/quotes/random")
-      .then((response) => response.json())
-      .then((data) => {
-        setRandomQuote(data[0].content);
-      })
-      .catch((error) => {
-        console.error("Error fetching quote:", error);
-      });
-  }
-
   return (
-    <div className="ms-5 me-5">
-      <SuperSEO
-        description="Ganjmo Ganjmo.com Ganjmo Ganjmo | React Super SEO Ganjmo Moreelganj Gorrelgan Ganjmo Comming Soon GANJMO ganjmo yes"
-        lang="en"
-        openGraph={{
-          ogImage: {
-            ogImage: "https://www.ganjmo.com/Ganjhat_logo_0-01.jpg",
-            ogImageAlt: "Ganjmo",
-            ogImageWidth: 1200,
-            ogImageHeight: 630,
-            ogImageType: "image/jpeg",
-          },
-        }}
-      />
-      <img className="mx-auto h-28 rounded-lg pt-2" src={gft} alt="" />
-      <h1 className="text-center font-serif  pt-3 text-xl md:text-3xl font-medium text-yellow-50 ">
-        {`Coming Soon Ganjmo`}
-      </h1>
-      <p className="text-center text-sm mt-2 text-yellow-50">
-        {`Welcome to Ganjmo! We're excited to announce that our enhanced e-commerce features are set to launch in just ${timeLeft.days} days, ${timeLeft.hours} hours, ${timeLeft.minutes} minutes, and ${timeLeft.seconds} seconds. Get ready for a seamless and powerful online shopping experience.`}
-      </p>
-      <div className=" mt-10 flex justify-center pb-5">
-        <div className="grid  grid-flow-col gap-5 text-center  auto-cols-max">
-          <div className="flex  text-orange-600 p-2 rounded-lg border border-double border-gray-600 bg-white  flex-col">
-            <span className="countdown font-mono text-5xl">
-              <span style={{ "--value": timeLeft.days }}></span>
-            </span>
-            days
-          </div>
-          <div className="flex  text-orange-600 p-2 rounded-lg border border-double border-gray-600 bg-white  flex-col">
-            <span className="countdown font-mono text-5xl">
-              <span style={{ "--value": timeLeft.hours }}></span>
-            </span>
-            hours
-          </div>
-          <div className="flex  text-orange-600 p-2 rounded-lg border border-double border-gray-600 bg-white  flex-col">
-            <span className="countdown font-mono text-5xl">
-              <span style={{ "--value": timeLeft.minutes }}></span>
-            </span>
-            min
-          </div>
-          <div className="flex  text-orange-600 p-2 rounded-lg border border-double border-gray-600 bg-white  flex-col">
-            <span className="countdown font-mono text-5xl">
-              <span style={{ "--value": timeLeft.seconds }}></span>
-            </span>
-            sec
-          </div>
+    <div>
+      <img className="h-14 mx-auto" src={logoGanjmo} alt="" />
+      <div>
+        <div className="border-2 mb-3 rounded-2xl border-t-0 rounded-t-none">
+          <h1 className="text-center pt-5 text-gray-100  text-xl p-5 pt-0 b md:text-2xl ">
+            {"গঞ্জমো শুভেচ্ছা সালামি,ঈদে গঞ্জমোর সাথে আনন্দ ভাগ করে নিন!"}
+          </h1>
+          <p className="text-center p-2 text-gray-100 text-sm">{`গঞ্জমোর "শুভেচ্ছা সালামি" ক্যাম্পেইনে অংশগ্রহণ করুন এবং আকর্ষণীয় পুরষ্কার জিতুন!`}</p>
         </div>
-      </div>
-      <p className="text-center font-bold font-mono text-yellow-50">{`End Date: ${timeLeft.endDate}`}</p>
-
-      {/* Display the random quote */}
-      <h4 className="text-center text-lg text-white mt-5">{randomQuote}</h4>
-      {/* Reload the image every 5 seconds */}
-      <img
-        className="mx-auto mt-5 h-80 md:h-96 rounded-2xl "
-        key={imageKey}
-        src={`https://random.imagecdn.app/500/150?${imageKey}`}
-        alt="Random Image"
-      />
-      <div className="text-yellow-50 space-y-3 pb-10">
-        <div className="flex  items-center justify-center gap-2 mt-10">
-          <img
-            className="h-24 rounded-full"
-            src={github?.avatar_url}
-            alt="Redowan Sajeeb Github avatar"
-          />
-          <div>
-            <h1 className="text-2xl ">{github.name}</h1>
-            <div>
-              <div className="mt-2 mb-2 space-x-3">
-                {/* <a target="blank" className=" text-white" href={github?.blog}>
-                  Blog🌍
-                </a> */}
-                <a
-                  target="blank"
-                  className=" text-white"
-                  // href={github?.html_url}
-                >
-                  gitHub Link👨‍💻
-                </a>
+        <div className="p-4 md:p-2">
+          <div className="card border-2 border-gray-700 md:w-96 mx-auto bg-base-100 shadow-xl">
+            <div className="card-body ">
+              <div className="space-y-5">
+                <p className="text-gray-500 text-sm font-semibold">{`"CHOOSE FIFLE" এ ক্লিক করে আপনার ছবি সিলেক্ট করে দিন`}</p>
+                <input
+                  type="file"
+                  className="file-input w-full file-input-bordered  max-w-xs"
+                />
+                <input
+                  type="text"
+                  placeholder="এখানে আপনার নাম লিখুন"
+                  className="input input-bordered w-full max-w-xs"
+                />
+                <button className="btn btn-outline w-full text-white bg-[#f56b0c]">
+                  আপনার পোস্টার তৈরি করুন
+                </button>
               </div>
-              <p className="text-sm text-gray-100">
-                Github followers: {github?.followers}
-              </p>
-              <p className="text-sm text-gray-100">
-                public_repos : {github?.public_repos}
-              </p>
-              <p className="text-sm text-gray-100">
-                twitter_username : {github?.twitter_username}
-              </p>
             </div>
           </div>
         </div>
+
+        <div className="p-4">
+          <div className="card  mx-auto md:w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <p className="text-center text-[#f56b0c]">
+                উদাহরণ: নিম্নলিখিত নকশা সহ পোস্টার
+              </p>
+              <img src={posterImg} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="mx-auto  pt-1 ps-5 mb-5 pe-5 m w-full">
+          <button className="btn w-full bg-[#fcfbfa] text-[#f56b0c] btn-outline">
+            ক্যাম্পেইন সম্পর্কে বিস্তারিত
+          </button>
+        </div>
+
+        <footer>
+          <p className="text-center text-sm text-white pb-5">
+            © 2024 Ganjmo.com Limited. All rights reserved.
+          </p>
+        </footer>
       </div>
-      <LinkPreview
-        url="https://www.ganjmo.com/Ganjhat_logo_text_1of.png"
-        width="400px"
-      />
     </div>
   );
 };
 
 export default App;
-
-// New features New Year Starts With New Year 2024 , yehIn Sha Allah
-// ENdependencies
